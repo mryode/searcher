49c545f222792e366023bee35490ba3ff25735b8
mryode <mryodedev@gmail.com>
2019-09-19 09:00:54 +0300

    add delete history record endpoint

--- backend/routes/videoHistory.js
+++ backend/routes/videoHistory.js
@@ -21,4 +21,10 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
+router.route('/:id').delete((req, res) => {
+    History.findByIdAndDelete(req.params.id)
+        .then(document => res.send(`${document} removed!`))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+})
+
 module.exports = router;
